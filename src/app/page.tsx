183c5a90d89500b8acf763cb1fdfebfaefb8a9c6
Eric Pezzulo <eric.pezzulo@gmail.com>
2023-06-25 22:06:48 -0400

    more SEO updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,15 @@ export const metadata = {
   title: "Eric Pezzulo",
   description:
     "Hi, I'm Eric and I'm a Full Stack Developer. I'm a Full Stack Develolper based in New York. I have 3 years professional experience and specialize in creating web app. I'm open for new opportunities and interesting projects",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
 };
 
 export default function Home() {
