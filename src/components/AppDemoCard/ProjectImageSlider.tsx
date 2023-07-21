03e7b798948e575dbb356dc10cea6458be47a417
Eric Pezzulo <eric.pezzulo@gmail.com>
2023-07-20 20:56:33 -0400

    updated projects card graphics

--- src/components/AppDemoCard/ProjectImageSlider.tsx
+++ src/components/AppDemoCard/ProjectImageSlider.tsx
@@ -1,28 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
+import "swiper/css";
 
 interface ImageSliderProps {
   images: string[];
 }
 
 const ProjectImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
-
   return (
-    <div className="h-40 overflow-hidden">
-    <Swiper
-    spaceBetween={0}
-    slidesPerView={1}>
-        {images.map((image, key)=> (
-            <SwiperSlide key={key} > 
-                <img className='object-contain flex w-full rounded' src={image} alt="image" /> 
-            </SwiperSlide> 
-        ))}
-    </Swiper>
+    <div className="">
+      <Swiper spaceBetween={0} slidesPerView={1}>
+        {images.length === 0 ? (
+          <div className="grid h-40 place-items-center rounded bg-gray-200">
+            <p className="text-gray-700">Graphics coming soon!</p>
+          </div>
+        ) : (
+          images.map((image, key) => (
+            <SwiperSlide key={key}>
+              <img
+                className="flex max-h-40 w-full rounded object-cover"
+                src={image}
+                alt="image"
+              />
+            </SwiperSlide>
+          ))
+        )}
+      </Swiper>
     </div>
   );
 };
 
-
-
-export default ProjectImageSlider
+export default ProjectImageSlider;
